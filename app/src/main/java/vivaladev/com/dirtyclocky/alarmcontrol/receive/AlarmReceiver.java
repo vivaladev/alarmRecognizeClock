fd11c7cc2d863e81a5dbb3d35d4e9e0ba998d1c5
GamesIS <IlyaK1997@gmail.com>
2018-07-02 00:29:56 +0400

    Alarm off

--- app/src/main/java/vivaladev/com/dirtyclocky/alarmcontrol/receive/AlarmReceiver.java
+++ app/src/main/java/vivaladev/com/dirtyclocky/alarmcontrol/receive/AlarmReceiver.java
@@ -3,18 +3,76 @@ package vivaladev.com.dirtyclocky.alarmcontrol.receive;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import vivaladev.com.dirtyclocky.ui.activities.AlarmClockActivity;
 
 
 public class AlarmReceiver extends BroadcastReceiver {
+    public static ArrayList<MiniAlarm> alarmsRunning = new ArrayList<MiniAlarm>();
     @Override
     public void onReceive(Context context, Intent intent) {
-        System.out.println("action = " + intent.getAction() + " " + "extra = " + intent.getStringExtra("extra"));
-        if (!AlarmClockActivity.isActive) {
+        int alarmID= intent.getIntExtra("requestCode", 1);
+
+        if(isNotRunningAlarm(alarmID) && !AlarmClockActivity.isActive){
+            if(!containsAlarmById(alarmID)){
+                alarmsRunning.add(new MiniAlarm(alarmID));
+            }
             Intent intentone = new Intent(context.getApplicationContext(), AlarmClockActivity.class);//Старт активти
+            intentone.putExtra("requestCode", String.valueOf(alarmID));
             intentone.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(intentone);
         }
+    }
+    public boolean isNotRunningAlarm(int alarmID){
+        for(MiniAlarm alarm: alarmsRunning){
+            if(alarm.getId() == alarmID && alarm.isRun()) {
+                return false;
+            }
+        }
+        return true;
+    }
+    class MiniAlarm{
+        private boolean run = true;
+        private int id;
+
+        public MiniAlarm(int id) {
+            this.id = id;
+        }
+
+        public boolean isRun() {
+            return run;
+        }
+
+        public void setRun(boolean run) {
+            this.run = run;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+    }
+
+    public static void setAlarmsOffByID(int alarmID){
+        for(MiniAlarm alarm: alarmsRunning){
+            if(alarm.getId() == alarmID){
+                alarm.setRun(false);
+                break;
+            }
+        }
+    }
+    public static boolean containsAlarmById(int alarmID){
+        for(MiniAlarm alarm: alarmsRunning){
+            if(alarm.getId() == alarmID){
+                return true;
+            }
+        }
+        return false;
     }
 }
