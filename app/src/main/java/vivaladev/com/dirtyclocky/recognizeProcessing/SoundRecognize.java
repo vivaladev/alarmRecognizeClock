f3d879fb5f3c8f75253c5d89380c8774881466d0
vivaladev <vivaladev.work23@gmail.com>
2018-07-02 00:56:14 +0400

    implemented compare for byte groups

--- app/src/main/java/vivaladev/com/dirtyclocky/recognizeProcessing/SoundRecognize.java
+++ app/src/main/java/vivaladev/com/dirtyclocky/recognizeProcessing/SoundRecognize.java
@@ -5,22 +5,30 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SoundRecognize {
 
     private static byte[] dbFileByte;
     private static byte[] newRecByte;
 
-    private int errorFullCoincidence = 20; //ошибка полного сравнения
-    private int errorComparisonOfHalves = 20; // ошибка сравнение половин
-    private int errorComprassionOfTree = 20; // ошибка сравнение по 3
-    private int errorComprassionOfFiveGroup = 20; // ошибка сравнение по 4
-    private int errorSearchCoincidenceByte = 20; // ошибка поиска похожих
+    private static int errorFullCoincidence = 20; //ошибка полного сравнения
+    private static int errorComparisonOfHalves = 20; // ошибка сравнение половин
+    private static int errorComprassionOfTree = 20; // ошибка сравнение по 3
+    private static int errorComprassionOfFiveGroup = 20; // ошибка сравнение по 4
+    private static int errorSearchCoincidenceByte = 20; // ошибка поиска похожих
+    private static int sizeCoincidenceByteGroup = 20; // ошибка поиска похожих
 
 
-    private static void initErrorConstant(){
-
+    public static void setErrorConstant(int erFull, int erHal, int erTree, int erFive, int erGroup) {
+        errorFullCoincidence = erFull; //ошибка полного сравнения
+        errorComparisonOfHalves = erHal; // ошибка сравнение половин
+        errorComprassionOfTree = erTree; // ошибка сравнение по 3
+        errorComprassionOfFiveGroup = erFive; // ошибка сравнение по 4
+        errorSearchCoincidenceByte = erGroup; // ошибка поиска похожих
     }
+
     /**
      * The method allows you to compare 2 files
      *
@@ -29,8 +37,6 @@ public class SoundRecognize {
     public static boolean recognizeSound(File dbFile, File newRec) {
         dbFileByte = convertFileToByteArray(dbFile);
         newRecByte = convertFileToByteArray(newRec);
-        initErrorConstant();
-
 
         //TODO: распознавать тут
         return true;
@@ -66,12 +72,11 @@ public class SoundRecognize {
                     }
                 }
             }
-            return true; // байты различны менее errorFullCoincidence
         } else {                                         //разная длина, просто пошаговое сравнение по меньшему массиву байт
             if (dbFileByte.length > newRecByte.length) {
                 //исходная запись дольше, чекаем все байты из меньшей
                 for (int i = 0; i < newRecByte.length; i++) {
-                    if(newRecByte[i] != dbFileByte[i]){
+                    if (newRecByte[i] != dbFileByte[i]) {
                         countError++;
                         if (countError >= errorFullCoincidence) {
                             return false; // байты различны более errorFullCoincidence, вернуть false
@@ -81,14 +86,73 @@ public class SoundRecognize {
             } else {
                 //исходная запись короче, чекаем все байты из бд
                 for (int i = 0; i < dbFileByte.length; i++) {
-                    if(dbFileByte[i] != newRecByte[i]){
+                    if (dbFileByte[i] != newRecByte[i]) {
                         countError++;
                         if (countError >= errorFullCoincidence) {
                             return false; // байты различны более errorFullCoincidence, вернуть false
                         }
                     }
                 }
-                return true;
+            }
+        }
+        return true;
+    }
+
+    private List<Byte> getByteList(byte[] array) {
+        List<Byte> res = new ArrayList<>();
+        for (byte b : array) {
+            res.add(b);
+        }
+        return res;
+    }
+
+    private boolean searchCoincedenceByte() {
+        List<Byte> dbFileList = getByteList(dbFileByte);
+        List<Byte> newRecList = getByteList(newRecByte);
+        int errorCount = 0;
+        //получили байт листы
+
+        if (dbFileList.size() > newRecList.size()) {
+            //исходная запись дольше, чекаем все байты из новой записи
+            for (int i = 0; i < newRecList.size(); i++) {
+                byte item = newRecList.get(i); // берем байт из активного листа
+                if (dbFileList.contains(item)) { // чекаем этот байт в другом листе
+                    boolean compareRes = compareTwoGroupOfByte(newRecList.subList(i, newRecList.size()),
+                            dbFileList.subList(dbFileList.indexOf(item), dbFileList.size())); // вызываем проверку группы одинаковых байтов
+                    if (!compareRes) {
+                        errorCount++;
+                        if (errorCount >= errorSearchCoincidenceByte) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        } else {
+            //исходная запись короче, чекаем все байты из бд
+            for (int i = 0; i < dbFileList.size(); i++) {
+                byte item = dbFileList.get(i); // берем байт из активного листа
+                if (newRecList.contains(item)) { // чекаем этот байт в другом листе
+                    boolean compareRes = compareTwoGroupOfByte(dbFileList.subList(i, dbFileList.size()),
+                            newRecList.subList(newRecList.indexOf(item), newRecList.size())); // вызываем проверку группы одинаковых байтов
+                    if (!compareRes) {
+                        errorCount++;
+                        if (errorCount >= errorSearchCoincidenceByte) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean compareTwoGroupOfByte(List<Byte> mainList, List<Byte> list2) {
+        if(!(mainList.size() >= sizeCoincidenceByteGroup && list2.size() >= sizeCoincidenceByteGroup)){
+            return false;
+        }
+        for (int i = 0; i < sizeCoincidenceByteGroup; i++) {
+            if(!mainList.get(i).equals(list2.get(i))){
+                return false;
             }
         }
         return true;
